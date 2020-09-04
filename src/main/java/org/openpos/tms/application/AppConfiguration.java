8969466f4b3e874ab78857fd3d961317b94caa00
Ilya Dubinsky <dubinsky.ilya@gmail.com>
2020-09-04 22:54:03 +0300

    Project skeleton

---
+++ src/main/java/org/openpos/tms/application/AppConfiguration.java
@@ -0,0 +1,8 @@
+package org.openpos.tms.application;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+	// TODO this will come from the config server
+}
