69df92b3d3da74ce35af17785d573faaa1093475
Ilya Dubinsky <dubinsky.ilya@gmail.com>
2020-09-05 12:02:19 +0300

    Added "production" flag

--- src/main/java/org/openpos/tms/application/AppConfiguration.java
+++ src/main/java/org/openpos/tms/application/AppConfiguration.java
@@ -1,8 +1,26 @@
 package org.openpos.tms.application;
 
 import org.springframework.context.annotation.Configuration;
+// TODO this will come from the config server
 
 @Configuration
 public class AppConfiguration {
-	// TODO this will come from the config server
+	// TODO for now this flag controls logging, need to check for an elegant
+	// solution to block logging of PCI-sensitive data
+	private boolean production;
+
+	/**
+	 * @return the production
+	 */
+	public boolean isProduction() {
+		return production;
+	}
+
+	/**
+	 * @param production the production to set
+	 */
+	public void setProduction(boolean production) {
+		this.production = production;
+	}
+
 }
